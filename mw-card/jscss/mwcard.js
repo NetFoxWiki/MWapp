e2ce90872934fd9bfec0446c2e0d647d344e9cad
网狐社 <yuansepailin@163.com>
2024-02-22 19:19:52 +0800

    m

--- mw-card/jscss/mwcard.js
+++ mw-card/jscss/mwcard.js
@@ -120,9 +120,6 @@ const Title = [
     '<b><font color="#ff9900">黄金大师</font></b>',
     '<b><font color="#ff9900">连队领航员</font></b>',
     '<b><font color="#931fff">传奇</font></b>',
-    '<b><font color="#931fff">教官</font></b>',
-    '<b><font color="#931fff">创作者</font></b>',
-    '<b><font color="#931fff">管理员</font></b>',
     '<b><font color="#931fff">神枪手</font></b>',
     '<b><font color="#931fff">财神爷</font></b>',
     '<b><font color="#931fff">祖灵之者</font></b>',
@@ -131,13 +128,20 @@ const Title = [
     '<b><font color="#931fff">奖牌得主</font></b>',
     '<b><font color="#931fff">冰霜狂欢</font></b>',
     '<b><font color="#931fff">滑雪能手</font></b>',
-    '<b><font color="#931fff">高级管理员</font></b>',
     '<b><font color="#931fff">年兽防御者</font></b>',
     '<b><font color="#931fff">圣诞老人助手</font></b>',
-    '<b><font color="#931fff">Discord 管理员</font></b>'
+
+]
+const TitleWork = [
+    '',
+    '<b><font color="#686868">游戏职位：</font><font color="#931fff">教官</font></b>',
+    '<b><font color="#686868">游戏职位：</font><font color="#931fff">创作者</font></b>',
+    '<b><font color="#686868">游戏职位：</font><font color="#931fff">管理员</font></b>',
+    '<b><font color="#686868">游戏职位：</font><font color="#931fff">高级管理员</font></b>',
+    '<b><font color="#686868">游戏职位：</font><font color="#931fff">Discord 管理员</font></b>'
 ]
 class Profile {
-    constructor(avatar, freeavatar, freeavatarQQ, name, title, gameid, gameidon, about, level, club, clubcolor, site) {
+    constructor(avatar, freeavatar, freeavatarQQ, name, title, titlework, gameid, gameidon, about, level, club, clubcolor, site, bgcolor) {
         if (title == 2 && avatar != 0 && avatar != 1) {
             this.avatar = Avatar[avatar] + '<div class="avatar-right"><img src="./title/PlayerRarityHero.png" width="50" height="100%" /></div>'
         } else if (title == 8 && avatar != 0 && avatar != 1) {
@@ -151,6 +155,7 @@ class Profile {
         this.freeavatarQQ = freeavatarQQ
         this.name = name
         this.title = Title[title]
+        this.titlework = TitleWork[titlework]
         if (gameid != "" && gameidon == "on") {
             this.gameid = gameid.substring(0, 7) + "****（隐私）"
         } else if (gameid != "") {
@@ -174,6 +179,7 @@ class Profile {
             this.clubcolor = Clubcolor[clubcolor]
         }
         this.site = Site[site]
+        this.bgcolor = bgcolor
     }
 }
 function showResult(profile) {
@@ -182,7 +188,7 @@ function showResult(profile) {
         if (profile.freeavatar != '') {
             if (profile.title28 == 2 && profile.titleavatar == 1) {
                 document.querySelector("#avatar").innerHTML = '<img class="avatar" src="' + profile.freeavatar + '" />' + '<div class="avatar-right"><img src="./title/PlayerRarityHero.png" width="50" height="100%" /></div>'
-            } else if (profile.title28 == 8 && profile.titleavatar == 1 ) {
+            } else if (profile.title28 == 8 && profile.titleavatar == 1) {
                 document.querySelector("#avatar").innerHTML = '<img class="avatar" src="' + profile.freeavatar + '" />' + '<div class="avatar-right"><img src="./title/PlayerRarityLegend.png" width="50" height="100%" /></div>'
             } else {
                 document.querySelector("#avatar").innerHTML = '<img class="avatar" src="' + profile.freeavatar + '" />'
@@ -190,7 +196,7 @@ function showResult(profile) {
         } else if (profile.freeavatarQQ != '') {
             if (profile.title28 == 2 && profile.titleavatar == 1) {
                 document.querySelector("#avatar").innerHTML = '<img class="avatar" src="https://q.qlogo.cn/headimg_dl?dst_uin=' + profile.freeavatarQQ + '&spec=640&img_type=jpg" />' + '<div class="avatar-right"><img src="./title/PlayerRarityHero.png" width="50" height="100%" /></div>'
-            } else if (profile.title28 == 8 && profile.titleavatar == 1 ) {
+            } else if (profile.title28 == 8 && profile.titleavatar == 1) {
                 document.querySelector("#avatar").innerHTML = '<img class="avatar" src="https://q.qlogo.cn/headimg_dl?dst_uin=' + profile.freeavatarQQ + '&spec=640&img_type=jpg" />' + '<div class="avatar-right"><img src="./title/PlayerRarityLegend.png" width="50" height="100%" /></div>'
             } else {
                 document.querySelector("#avatar").innerHTML = '<img class="avatar" src="https://q.qlogo.cn/headimg_dl?dst_uin=' + profile.freeavatarQQ + '&spec=640&img_type=jpg" />'
@@ -198,7 +204,7 @@ function showResult(profile) {
         } else {
             if (profile.title28 == 2 && profile.titleavatar == 1) {
                 document.querySelector("#avatar").innerHTML = '<img class="avatar" src="./Avatar/info.png" />' + '<div class="avatar-right"><img src="./title/PlayerRarityHero.png" width="50" height="100%" /></div>'
-            } else if (profile.title28 == 8 && profile.titleavatar == 1 ) {
+            } else if (profile.title28 == 8 && profile.titleavatar == 1) {
                 document.querySelector("#avatar").innerHTML = '<img class="avatar" src="./Avatar/info.png" />' + '<div class="avatar-right"><img src="./title/PlayerRarityLegend.png" width="50" height="100%" /></div>'
             } else {
                 document.querySelector("#avatar").innerHTML = '<img class="avatar" src="./Avatar/info.png" />'
@@ -209,6 +215,7 @@ function showResult(profile) {
     }
     document.querySelector("#name").innerText = profile.name
     document.querySelector("#title").innerHTML = profile.title
+    document.querySelector("#titlework").innerHTML = profile.titlework
     document.querySelector("#gameid").innerText = profile.gameid
     document.querySelector("#about").innerHTML = '<font color="#686868">最擅长舰种：</font>' + profile.about
     if (profile.level <= 30) {
@@ -220,6 +227,7 @@ function showResult(profile) {
         document.querySelector("#club").innerHTML = '<font color="#686868">所在舰队：</font><font color="' + profile.clubcolor + '">' + profile.club + '</font>'
     }
     document.querySelector("#site").innerHTML = profile.site
+    document.getElementById("bgcolor").style.backgroundColor = profile.bgcolor
     document.querySelector("#link").innerHTML = '<input type="text" value="' + window.location.href + '" readonly />'
 }
 function tryGetParam(params, prop) {
@@ -236,7 +244,6 @@ async function savePicture() {
     }).then(
         async function (canvas) {
             canvas = canvas.toDataURL("image/png", 1);
-            console.log(canvas);
             document.getElementById("download").setAttribute("href", canvas);
             document.getElementById("download").click();
         }
@@ -249,12 +256,14 @@ let prof = new Profile(
     tryGetParam(urlParams, 'freeavatarQQ'),
     tryGetParam(urlParams, 'name'),
     tryGetParam(urlParams, 'title'),
+    tryGetParam(urlParams, 'titlework'),
     tryGetParam(urlParams, 'gameid'),
     tryGetParam(urlParams, 'gameidon'),
     tryGetParam(urlParams, 'about'),
     tryGetParam(urlParams, 'level'),
     tryGetParam(urlParams, 'club'),
     tryGetParam(urlParams, 'clubcolor'),
-    tryGetParam(urlParams, 'site')
+    tryGetParam(urlParams, 'site'),
+    tryGetParam(urlParams, 'bgcolor'),
 )
 showResult(prof)
